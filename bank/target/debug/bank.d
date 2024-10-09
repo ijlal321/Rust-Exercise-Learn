22451a633e5205983b4646254b07d436d12eba51
IJLAL <ijlal.tanveer294@gmail.com>
2024-10-09 17:32:36 +0500

    started Bank Project

---
+++ bank/target/debug/bank.d
@@ -0,0 +1,1 @@
+E:\Github\ Projects\Rust-Exercise-Learn\bank\target\debug\bank.exe: E:\Github\ Projects\Rust-Exercise-Learn\bank\src\main.rs
---
+++ bank/target/debug/deps/bank.d
@@ -0,0 +1,5 @@
+E:\Github Projects\Rust-Exercise-Learn\bank\target\debug\deps\bank.exe: src/main.rs
+
+E:\Github Projects\Rust-Exercise-Learn\bank\target\debug\deps\bank.d: src/main.rs
+
+src/main.rs:
