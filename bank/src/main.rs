c938b38f989d5098afd469673220ae115bb882b5
IJLAL <ijlal.tanveer294@gmail.com>
2024-10-10 21:51:23 +0500

    project done

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,40 +1,69 @@
-
 #[derive(Debug)]
-struct Account{
+struct Account {
     id: u32,
     balance: i32,
     holder: String,
 }
 
-impl Account{
-    fn new(id: u32, holder: String) -> Self{
-        Account{
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
             id,
             holder,
-            balance:0,
+            balance: 0,
         }
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
     }
 }
 
 #[derive(Debug)]
-struct Bank{
+struct Bank {
     accounts: Vec<Account>,
 }
 
-impl Bank{
-    fn new() -> Self{
-        return Bank {accounts: vec![]};
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
     }
 
-    fn add_account(&mut self, account: Account){
+    fn add_account(&mut self, account: Account) {
         self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
     }
 }
 
 fn main() {
     let mut bank = Bank::new();
-    let account = Account::new(1, String::from("me"));
+    let mut account = Account::new(1, String::from("me"));
+
+    account.deposit(500);
+    account.withdraw(250);
 
     bank.add_account(account);
-    println!("{:#?}", bank);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
