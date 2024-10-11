7313f24948c2067474dc6de245c3bcbe5bc52070
IJLAL <ijlal.tanveer294@gmail.com>
2024-10-11 12:50:04 +0500

    started new project Media

---
+++ media/target/debug/deps/media.d
@@ -0,0 +1,5 @@
+E:\Github Projects\Rust-Exercise-Learn\media\target\debug\deps\media.exe: src/main.rs
+
+E:\Github Projects\Rust-Exercise-Learn\media\target\debug\deps\media.d: src/main.rs
+
+src/main.rs:
---
+++ media/target/debug/media.d
@@ -0,0 +1,1 @@
+E:\Github\ Projects\Rust-Exercise-Learn\media\target\debug\media.exe: E:\Github\ Projects\Rust-Exercise-Learn\media\src\main.rs
